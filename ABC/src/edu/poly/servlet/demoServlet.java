8e70ae62e0485eacd18436a4c11346f9dd165e74
NguyenNhatNam22072002 <nhatnam22072002@gmail.com>
2022-12-04 00:48:26 +0700

    demo

--- ABC/src/edu/poly/servlet/demoServlet.java
+++ ABC/src/edu/poly/servlet/demoServlet.java
@@ -36,6 +36,7 @@ public class demoServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		//IRIRIR
 	}
 
 }
